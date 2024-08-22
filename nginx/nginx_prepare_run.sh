b205802bcab36fd23eca287fa4f473deb867d51e
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2024-08-22 23:30:08 +0300

    fix: bckp.s-ho.me cert

--- nginx/nginx_prepare_run.sh
+++ nginx/nginx_prepare_run.sh
@@ -2,4 +2,5 @@
 envsubst '${PLUTCHART_DOMAIN} ${SHOME_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst '${PLUTCHART_DOMAIN}' < /etc/nginx/plutchart_ssl.conf.template > /etc/nginx/plutchart_ssl.conf
 envsubst '${SHOME_DOMAIN}' < /etc/nginx/shome_ssl.conf.template > /etc/nginx/shome_ssl.conf
+envsubst '${SHOME_DOMAIN}' < /etc/nginx/bckp_ssl.conf.template > /etc/nginx/bckp_ssl.conf
 nginx -g "daemon off;"
