a5eff8c5953e03b1214a643ed0705034a5697436
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2025-02-05 12:38:43 +0300

    s-ho.me disapear

--- certbot/launch.sh
+++ certbot/launch.sh
@@ -1,3 +1,6 @@
 # !!!! DON'T RUN unconsiously
 docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d example.org
-docker compose run --rm certbot renew --dry-run
+docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --cert-name plutchart.com --dry-run -d sh.plutchart.com,bckp.plutchart.com
+docker compose run --rm certbot renew --cert-name plutchart.com --webroot-path /var/www/certbot/
+docker compose run --rm certbot renew --cert-name s-ho.me --webroot-path /var/www/certbot/ --dry-run
+docker compose run --rm certbot renew --cert-name s-ho.me-0001 --webroot-path /var/www/certbot/ --dry-run
